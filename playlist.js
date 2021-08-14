9f0d591be322845ea9d4ce1d8d5408124bed8666
JoyDragonX <57489277+JoyDragonX@users.noreply.github.com>
2021-08-15 04:10:08 +0800

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -127,5 +127,318 @@ trackList =
             artist: "MAN WITH A MISSION",
             src: "https://joydragonx.github.io/music/日本歌/MAN WITH A MISSIONWinding Road  黃金神威OP 中日英字幕.mp3",
             lrc: "https://joydragonx.github.io/music/lyric/MAN WITH A MISSIONWinding Road  黃金神威OP 中日英字幕.lrc"
-        }
+        },
+        {
+            name: "Sayuri×MY FIRST STORY - レイメイ 黃金神威 OP2",
+            artist: "Sayuri×MY FIRST STORY",
+            src: "https://joydragonx.github.io/music/日本歌/黃金神威 第二季OP「レイメイ」.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/黃金神威 第二季OP「レイメイ」.lrc"
+        },
+        {
+            name: "FOMARE - Grey 黃金神威 OP3",
+            artist: "FOMARE",
+            src: "https://joydragonx.github.io/music/日本歌/黃金神威Op3 - 『Grey』.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/黃金神威Op3 - 『Grey』.lrc"
+        },
+        {
+            name: "Eastern Youth - 時計台の鐘 黃金神威 ED2",
+            artist: "Eastern Youth",
+            src: "https://joydragonx.github.io/music/日本歌/「時計台の鐘」黃金神威ed2.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/「時計台の鐘」黃金神威ed2.lrc"
+        },
+        {
+            name: "THE SIXTH LIE - 融雪 黃金神威 ED3",
+            artist: "THE SIXTH LIE",
+            src: "https://joydragonx.github.io/music/日本歌/黃金神威 3 ED_Ending Full - Yuusetsu.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/黃金神威 3 ED_Ending Full - Yuusetsu.lrc" 
+        },
+        {
+            name: "田所梓 - DEAREST DROP  末日時在做什麼？有沒有空？可以來拯救嗎？ OP1",
+            artist: "田所梓",
+            src: "https://joydragonx.github.io/music/日本歌/末日時op.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/末日時op.lrc"
+        },
+        {
+            name: "TRUE - フロム 末日時在做什麼？有沒有空？可以來拯救嗎？ ED1",
+            artist: "TRUE",
+            src: "https://joydragonx.github.io/music/日本歌/TRUE フロム 末日時.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/TRUE フロム 末日時.lrc"
+        },
+        {
+            name: "山田珠露 - Scarborough Fair 末日時在做什麼？有沒有空？可以來拯救嗎？ OST1",
+            artist: "山田珠露",
+            src: "https://joydragonx.github.io/music/日本歌/Scarborough Fair史卡博羅市集山田タマル.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/Scarborough Fair史卡博羅市集山田タマル.lrc"
+        },
+        {
+            name: "加藤達也 - Always in my heart 末日時在做什麼？有沒有空？可以來拯救嗎？ OST2",
+            artist: "加藤達也",
+            src: "https://joydragonx.github.io/music/日本歌/末日時在做什麼插曲Always in my heart.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/末日時在做什麼插曲Always in my heart.lrc" // 16
+        },
+        {
+            name: "ChouCho - 優しさの理由 冰菓 OP",
+            artist: "ChouCho",
+            src: "https://joydragonx.github.io/music/日本歌/古典部冰菓op 優しさの理由.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/古典部冰菓op 優しさの理由.lrc"
+        },
+        {
+            name: "千反田愛瑠（佐藤聰美）、伊原摩耶花（茅野愛衣） - まどろみの約束 冰菓 ED",
+            artist: "千反田愛瑠（佐藤聰美）、伊原摩耶花（茅野愛衣）",
+            src: "https://joydragonx.github.io/music/日本歌/古典部冰菓ED まどろみの約束.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/古典部冰菓ED まどろみの約束.lrc"
+        },
+        {
+            name: "TRUE - Sincerely 紫羅蘭永恆花園 OP",
+            artist: "TRUE Sincerely",
+            src: "https://joydragonx.github.io/music/日本歌/TRUE Sincerely violetevergarden Opning.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/TRUE Sincerely violetevergarden Opning.lrc"
+        },
+        {
+            name: "茅原實里 - みちしるべ 紫羅蘭永恆花園 ED",
+            artist: "茅原實里",
+            src: "https://joydragonx.github.io/music/日本歌/紫羅蘭永恆花園片尾曲茅原實里みちしるべ.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/紫羅蘭永恆花園片尾曲茅原實里みちしるべ.lrc"
+        },
+        {
+            name: "TRUE - LETTER 紫羅蘭永恆花園 OST",
+            artist: "TRUE",
+            src: "https://joydragonx.github.io/music/日本歌/紫羅蘭 LETTER.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/紫羅蘭 LETTER.lrc" // 21
+        },
+        {
+            name: "YOASOBI - 夜に駆ける",
+            artist: "YOASOBI",
+            src: "https://joydragonx.github.io/music/日本歌/YOASOBI「夜に駆ける」 Official Music Video.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/YOASOBI「夜に駆ける」 Official Music Video.lrc"
+        },
+        {
+            name: "本間芽衣子（茅野愛衣）、安城鳴子（戶松遙）、鶴見知利子（早見沙織） - secret base ～君がくれたもの～（10 years after Ver.） 我們仍未知道那天所看見的花名 ED",
+            artist: "本間芽衣子（茅野愛衣）、安城鳴子（戶松遙）、鶴見知利子（早見沙織）",
+            src: "https://joydragonx.github.io/music/日本歌/secret base Kimi ga Kureta Mono 10 years after ver 未聞花名ed.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/secret base Kimi ga Kureta Mono 10 years after ver 未聞花名ed.lrc"
+        },
+        {
+            name: "黒木渚 - カルデラ",
+            artist: "黒木渚",
+            src: "https://joydragonx.github.io/music/日本歌/《カルデラ》黒木渚.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/《カルデラ》黒木渚.lrc"
+        },
+        {
+            name: "LiSA - 一番の寶物 Angel Beats OST",
+            artist: "LiSA",
+            src: "https://joydragonx.github.io/music/日本歌/【HD】Angel Beats - LiSA - 一番の寶物【中日字幕】.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/【HD】Angel Beats - LiSA - 一番の寶物【中日字幕】.lrc"
+        },
+        {
+            name: "Aimer - 六等星の夜",
+            artist: "Aimer",
+            src: "https://joydragonx.github.io/music/日本歌/【HD】ONE_花の唄_六等星の夜 Magic Blue ver. - Aimer - 六等星の夜 Magic Blue ver.【中日字幕】.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/【HD】ONE_花の唄_六等星の夜 Magic Blue ver. - Aimer - 六等星の夜 Magic Blue ver.【中日字幕】.lrc"
+        },
+        {
+            name: "Aimer - 悲しみの向こう側",
+            artist: "Aimer",
+            src: "https://joydragonx.github.io/music/日本歌/【HD】SPARK-AGAIN - Aimer - 悲しみの向こう側【中日字幕】.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/【HD】SPARK-AGAIN - Aimer - 悲しみの向こう側【中日字幕】.lrc"
+        },
+        {
+            name: "Aimer - Ref:rain 愛在雨過天晴時 ED",
+            artist: "Aimer",
+            src: "https://joydragonx.github.io/music/日本歌/【HD】愛在雨過天晴時 _ 戀如雨止 ED - Aimer - Ref_rain【中日字幕】.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/【HD】愛在雨過天晴時 _ 戀如雨止 ED - Aimer - Ref_rain【中日字幕】.lrc"
+        },
+        {
+            name: "ウォルピスカーター - 泥中に咲く",
+            artist: "ウォルピスカーター",
+            src: "https://joydragonx.github.io/music/日本歌/ウォルピスカーター MV 『泥中に咲く』.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/ウォルピスカーター MV 『泥中に咲く』.lrc"
+        },
+        {
+            name: "鹿乃 - この世界を愛したい (好想愛這個世界啊)",
+            artist: "鹿乃",
+            src: "https://joydragonx.github.io/music/日本歌/この世界を愛したい cover.鹿乃.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/この世界を愛したい cover.鹿乃.lrc"
+        },
+        {
+            name: "Goose house - 光るなら（若能綻放光芒） 四月是你的謊言 OP1",
+            artist: "",
+            src: "https://joydragonx.github.io/music/日本歌/四月是你的謊言 OP1 光るなら.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/四月是你的謊言 OP1 光るなら.lrc"
+        },
+        {
+            name: "Coalamode - 七色シンフォニー（彩虹一般的交響曲） 四月是你的謊言 OP2",
+            artist: "Coalamode",
+            src: "https://joydragonx.github.io/music/日本歌/四月是你的謊言 OP2 七色シンフォニー.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/四月是你的謊言 OP2 七色シンフォニー.lrc"
+        },
+        {
+            name: "wacci - キラメキ（閃耀） 四月是你的謊言 ED1",
+            artist: "wacci",
+            src: "https://joydragonx.github.io/music/日本歌/キラメキ四月是你的謊言ed.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/キラメキ四月是你的謊言ed.lrc"
+        },
+        {
+            name: "7!! - オレンジ（橙色）",
+            artist: "7!!",
+            src: "https://joydragonx.github.io/music/日本歌/四月是你的謊言ed オレンジ.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/四月是你的謊言ed オレンジ.lrc"  // 34
+        },
+        {
+            name: "女王蜂 - 火炎 多羅羅 OP",
+            artist: "女王蜂",
+            src: "https://joydragonx.github.io/music/日本歌/多羅羅OP 女王蜂 火 中日歌詞.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/多羅羅OP 女王蜂 火 中日歌詞.lrc"
+        },
+        {
+            name: "amazarashi - さよならごっこ 多羅羅 ED",
+            artist: "amazarashi",
+            src: "https://joydragonx.github.io/music/日本歌/多羅羅ED amazarashi さよならごっこ 中日歌詞.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/多羅羅ED amazarashi さよならごっこ 中日歌詞.lrc"
+        },
+        {
+            name: "中澤伴行 - here and there 奇諾之旅 OP",
+            artist: "中澤伴行",
+            src: "https://joydragonx.github.io/music/日本歌/奇諾之旅 OP here and there.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/奇諾之旅 OP here and there.lrc"
+        },
+        {
+            name: "出羽良彰 - 砂糖玉の月 奇諾之旅 ED",
+            artist: "出羽良彰",
+            src: "https://joydragonx.github.io/music/日本歌/奇諾之旅 ED 砂糖玉の月.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/奇諾之旅 ED 砂糖玉の月.lrc"    // 38
+        },
+        {
+            name: "田中望（赤崎千夏）、菊池茜（戶松遙）、鷺宮詩織（豐崎愛生） -  青春のリバーブ 女高中生的虚度日常 ED",
+            artist: "田中望（赤崎千夏）、菊池茜（戶松遙）、鷺宮詩織（豐崎愛生）",
+            src: "https://joydragonx.github.io/music/日本歌/女高中生的虚度日常 ED 青春のリバーブ.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/女高中生的虚度日常 ED 青春のリバーブ.lrc"
+        },
+        {
+            name: "玉木真理（水瀨祈）、小淵澤報瀨（花澤香菜）、三宅日向（井口裕香）、白石結月（早見沙織） - ここからここから 宇宙よりも遠い場所 ED",
+            artist: "玉木真理（水瀨祈）、小淵澤報瀨（花澤香菜）、三宅日向（井口裕香）、白石結月（早見沙織）",
+            src: "https://joydragonx.github.io/music/日本歌/宇宙よりも遠い場所 ED Full ここからここから.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/宇宙よりも遠い場所 ED Full ここからここから.lrc"
+        },
+        {
+            name: "saya - ハルカトオク 比宇宙更遠的地方 OST",
+            artist: "saya",
+            src: "https://joydragonx.github.io/music/日本歌/宇宙よりも遠い場所 挿入歌ハルカトオクSora yori mo Tooi Basho.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/宇宙よりも遠い場所 挿入歌ハルカトオクSora yori mo Tooi Basho.lrc"
+        },
+        {
+            name: "saya - またね 比宇宙更遠的地方 OST",
+            artist: "saya",
+            src: "https://joydragonx.github.io/music/日本歌/宇宙よりも遠い場所 挿入歌またねSora yori mo Tooi Basho.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/宇宙よりも遠い場所 挿入歌またねSora yori mo Tooi Basho.lrc"
+        },
+        {
+            name: "海野高校海堤部（鶴木陽渚（高尾奏音）、 帆高夏海（川井田夏海）、黑岩悠希（篠原侑）、大野真（明坂聰美）） - SEA HORIZON 放學後堤防日誌 OP",
+            artist: "海野高校海堤部（鶴木陽渚（高尾奏音）、 帆高夏海（川井田夏海）、黑岩悠希（篠原侑）、大野真（明坂聰美））",
+            src: "https://joydragonx.github.io/music/日本歌/放學後堤防日誌 OP SEA HORIZON.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/放學後堤防日誌 OP SEA HORIZON.lrc"
+        },
+        {
+            name: "村川梨衣 - Distance 極道超女 ED",
+            artist: "村川梨衣",
+            src: "https://joydragonx.github.io/music/日本歌/村川梨衣 Distance 極道超女ed.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/村川梨衣 Distance 極道超女ed.lrc"
+        },
+        {
+            name: "富田美憂 - Broken Sky 無能力者娜娜 OP",
+            artist: "富田美憂",
+            src: "https://joydragonx.github.io/music/日本歌/無能力者娜娜『OP』 Broken Sky.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/無能力者娜娜『OP』 Broken Sky.lrc"
+        },
+        {
+            name: "藤川千愛 - バケモノと呼ばれて (被稱之為怪物) 無能力者娜娜 ED",
+            artist: "藤川千愛",
+            src: "https://joydragonx.github.io/music/日本歌/無能力者娜娜『ED』バケモノと呼ばれて (被稱之為怪物).mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/無能力者娜娜『ED』バケモノと呼ばれて (被稱之為怪物).lrc"  //46
+        },
+        {
+            name: "湘南乃風 - Grand Blue 碧藍之海 OP",
+            artist: "湘南乃風",
+            src: "https://joydragonx.github.io/music/日本歌/碧藍之海 OP主題歌 湘南乃風Grand Blue.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/碧藍之海 OP主題歌 湘南乃風Grand Blue.lrc"
+        },
+        {
+            name: "奥華子 - ガーネット 穿越時空的少女 OP",
+            artist: "",
+            src: "https://joydragonx.github.io/music/日本歌/穿越時空的少女 主題曲 奥華子 - 「 ガーネット 」.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/穿越時空的少女 主題曲 奥華子 - 「 ガーネット 」.lrc"
+        },
+        {
+            name: "千石撫子（花澤香菜） - 恋愛サーキュレーション（戀愛循環） 化物語 OP",
+            artist: "千石撫子（花澤香菜）",
+            src: "https://joydragonx.github.io/music/日本歌/Renai Circulation恋愛サーキュレーション歌ってみたなみりん.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/Renai Circulation恋愛サーキュレーション歌ってみたなみりん.lrc"
+        },
+        {
+            name: "羽川翼（堀江由衣） - Sugar Sweet Nightmare 化物語 OP",
+            artist: "羽川翼（堀江由衣）",
+            src: "https://joydragonx.github.io/music/日本歌/Sugar Sweet Nightmare.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/Sugar Sweet Nightmare.lrc"
+        },
+        {
+            name: "nagi - 君の知らない物語 （你不知道的故事） 化物語 ED",
+            artist: "nagi",
+            src: "https://joydragonx.github.io/music/日本歌/supercell Kimino Shiranai Monogatari.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/supercell Kimino Shiranai Monogatari.lrc"
+        },
+        {
+            name: "Mili - Rightfully 哥布林殺手 OP",
+            artist: "Mili",
+            src: "https://joydragonx.github.io/music/日本歌/哥布林殺手Goblin Slayer OP Mili Rightfully中英字幕.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/哥布林殺手Goblin Slayer OP Mili Rightfully中英字幕.lrc"
+        },
+        {
+            name: "蒼井翔太 - BAD END 轉生成女性向遊戲只有毀滅END的壞人大小姐 ED",
+            artist: "蒼井翔太",
+            src: "https://joydragonx.github.io/music/日本歌/蒼井翔太 BAD END.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/蒼井翔太 BAD END.lrc"
+        },
+        {
+            name: "HAPPY BIRTHDAY - 君だったら",
+            artist: "HAPPY BIRTHDAY",
+            src: "https://joydragonx.github.io/music/日本歌/超好聽的日語歌曲——《君だったら》HAPPY BIRTHDAY.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/超好聽的日語歌曲——《君だったら》HAPPY BIRTHDAY.lrc"     // 54
+        },
+        {
+            name: "flumpool - ちいさな日々 隱瞞之事 OP",
+            artist: "flumpool",
+            src: "https://joydragonx.github.io/music/日本歌/隱瞞之事 OP Full ちいさな日々 flumpool.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/隱瞞之事 OP Full ちいさな日々 flumpool.lrc"
+        },
+        {
+             name: "中川奈美 - 竈門炭治郎のうた 鬼滅之刃 ED",
+            artist: "中川奈美",
+            src: "https://joydragonx.github.io/music/日本歌/鬼滅之刃 EP.19ED 中川奈美 -「 竈門炭治郎のうた 」【中日歌詞】.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/鬼滅之刃 EP.19ED 中川奈美 -「 竈門炭治郎のうた 」【中日歌詞】.lrc"
+        },
+        {
+            name: "逢坂大河、櫛枝實乃梨、川嶋亞美（釘宮理惠、堀江由衣、喜多村英梨） - プレパレード （戀愛預演） 龍與虎 OP",
+            artist: "逢坂大河、櫛枝實乃梨、川嶋亞美（釘宮理惠、堀江由衣、喜多村英梨）",
+            src: "https://joydragonx.github.io/music/日本歌/プレパレード - 龍與虎.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/プレパレード - 龍與虎.lrc"
+        },
+        {
+            name: "菅原紗由理 - キミに赠る歌",
+            artist: "菅原紗由理",
+            src: "https://joydragonx.github.io/music/日本歌/一首好聽的日語歌《キミに赠る歌》菅原紗由理【中日歌詞Lyrics】.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/一首好聽的日語歌《キミに赠る歌》菅原紗由理【中日歌詞Lyrics】.lrc"
+        },
+        {
+            name: "中島美嘉 - 僕が死のうと思ったのは",
+            artist: "中島美嘉",
+            src: "https://joydragonx.github.io/music/日本歌/僕が死のうと思ったのは.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/僕が死のうと思ったのは.lrc"
+        },
+        {
+            name: "あいみょん - 空の青さを知る人よ 知道天空有多藍的人啊 OP",
+            artist: "",
+            src: "https://joydragonx.github.io/music/日本歌/動畫電影【知道天空有多藍的人啊】主題曲 あいみょん -「 空の青さを知る人よ 」【中日歌詞】.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/動畫電影【知道天空有多藍的人啊】主題曲 あいみょん -「 空の青さを知る人よ 」【中日歌詞】.lrc"
+        },
+        
     ];
