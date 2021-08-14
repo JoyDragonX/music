4150993234120ae386e7e56ef040df643a9b1608
JoyDragonX <57489277+JoyDragonX@users.noreply.github.com>
2021-08-15 00:02:14 +0800

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -77,5 +77,53 @@ trackList =
             artist: "蘇打綠",
             src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【近未來】Official Music Video.mp3",
             lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【近未來】Official Music Video.lrc"
+        },
+        {
+            name: "牧野由依 - Reset 重啟咲良田 OP1",
+            artist: "牧野由依",
+            src: "https://joydragonx.github.io/music/日本歌/牧野由依 Reset.mp3",
+            lrc: "https://joydragonx.github.io/music/日本歌/牧野由依 Reset.lrc"
+        },
+        {
+            name: "WEAVER - だから僕は僕を手放す 重啟咲良田 OP2",
+            artist: "WEAVER",
+            src: "https://joydragonx.github.io/music/日本歌/Sakurada Reset OP2WEAVER  Dakara Boku wa Boku wo Tebanasuだから僕は僕を手放す.mp3",
+            lrc: "https://joydragonx.github.io/music/日本歌/Sakurada Reset OP2WEAVER  Dakara Boku wa Boku wo Tebanasuだから僕は僕を手放す.lrc"
+        },
+        {
+            name: "山中拓也 - トナリアウ 重啟咲良田 ED1",
+            artist: "山中拓也",
+            src: "https://joydragonx.github.io/music/日本歌/トナリアウSakurada Reset ED.mp3",
+            lrc: "https://joydragonx.github.io/music/日本歌/.lrc"
+        },
+        {
+            name: "牧野由依 - Colors of happiness 重啟咲良田 ED2",
+            artist: "牧野由依",
+            src: "https://joydragonx.github.io/music/日本歌/Colors of happiness Sakurada Reset ED.mp3",
+            lrc: "https://joydragonx.github.io/music/日本歌/Colors of happiness Sakurada Reset ED.lrc"
+        },
+        {
+            name: "中井雅子 - もうすでに失ったもの 重啟咲良田OST",
+            artist: "中井雅子",
+            src: "https://joydragonx.github.io/music/日本歌/Sakurada Reset Track 02 What Ive Lost Already Rayons もうすでに失ったもの 重啟咲良田.mp3",
+            lrc: "https://joydragonx.github.io/music/日本歌/Sakurada Reset Track 02 What Ive Lost Already Rayons もうすでに失ったもの 重啟咲良田.lrc"
+        },
+        {
+            name: "鈴木このみ - Theater of Life DECA-DENCE OP",
+            artist: "鈴木このみ",
+            src: "https://joydragonx.github.io/music/日本歌/DECADENCE OP FullAMV Theater of Lifeby Konomi Suzuki HD.mp3",
+            lrc: "https://joydragonx.github.io/music/日本歌/DECADENCE OP FullAMV Theater of Lifeby Konomi Suzuki HD.lrc"
+        },
+        {
+            name: "伊東歌詞太郎 - 記憶の箱舟",
+            artist: "伊東歌詞太郎",
+            src: "https://joydragonx.github.io/music/日本歌/伊東歌詞太郎記憶の箱舟TVアニメデカダンスEDテーマMusic Video.mp3",
+            lrc: "https://joydragonx.github.io/music/日本歌/伊東歌詞太郎記憶の箱舟TVアニメデカダンスEDテーマMusic Video.lrc"
+        },
+        {
+            name: "MAN WITH A MISSION - Winding Road 黃金神威 OP1",
+            artist: "MAN WITH A MISSION",
+            src: "https://joydragonx.github.io/music/日本歌/MAN WITH A MISSIONWinding Road  黃金神威OP 中日英字幕.mp3",
+            lrc: "https://joydragonx.github.io/music/日本歌/MAN WITH A MISSIONWinding Road  黃金神威OP 中日英字幕.lrc"
         }
     ];
