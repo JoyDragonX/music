ad5dc47d4fc92ffa57570bca540cae2a6dc70a0f
JoyDragonX <57489277+JoyDragonX@users.noreply.github.com>
2021-08-15 00:14:39 +0800

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -10,120 +10,120 @@ trackList =
             name: "吳青峰 - 起風了",
             artist: "吳青峰",
             src: "https://joydragonx.github.io/music/中文歌/吳青峰《起風了》高音質 動態歌詞版MV.mp3",
-            lrc: "https://joydragonx.github.io/music/中文歌/吳青峰《起風了》高音質 動態歌詞版MV.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/吳青峰《起風了》高音質 動態歌詞版MV.lrc"
         },
         {
             name: "蘇打綠 - 小情歌",
             artist: "蘇打綠",
             src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen - 【小情歌 (Endless Story Live) 】 Official Music Video.mp3",
-            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen - 【小情歌 (Endless Story Live) 】 Official Music Video.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/蘇打綠 sodagreen - 【小情歌 (Endless Story Live) 】 Official Music Video.lrc"
         },
         {
             name: "蘇打綠 - 下雨的夜晚",
             artist: "蘇打綠",
             src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【下雨的夜晚 Live】Official Music Video.mp3",
-            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【下雨的夜晚 Live】Official Music Video.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/蘇打綠 sodagreen -【下雨的夜晚 Live】Official Music Video.lrc"
         },
         {
             name: "蘇打綠 - 你在煩惱什麼",
             artist: "蘇打綠",
             src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【你在煩惱什麼 Whats The Trouble On Your Mind】Official Music Video.mp3",
-            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【你在煩惱什麼 Whats The Trouble On Your Mind】Official Music Video.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/蘇打綠 sodagreen -【你在煩惱什麼 Whats The Trouble On Your Mind】Official Music Video.lrc"
         },
         {
             name: "蘇打綠 - 再遇見",
             artist: "蘇打綠",
             src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【再遇見】Official Music Video.mp3.mp3",
-            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【再遇見】Official Music Video.mp3.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/蘇打綠 sodagreen -【再遇見】Official Music Video.mp3.lrc"
         },
         {
             name: "蘇打綠 sodagreen -【十年一刻】Official Music Video.mp3",
             artist: "蘇打綠",
             src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【十年一刻】Official Music Video.mp3.mp3",
-            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【十年一刻】Official Music Video.mp3.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/蘇打綠 sodagreen -【十年一刻】Official Music Video.mp3.lrc"
         },
         {
             name: "蘇打綠 - 左邊",
             artist: "蘇打綠",
             src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【左邊】Official Music Video.mp3",
-            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【左邊】Official Music Video.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/蘇打綠 sodagreen -【左邊】Official Music Video.lrc"
         },
         {
             name: "蘇打綠 - 幸福額度",
             artist: "蘇打綠",
             src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【幸福額度】Official Music Video.mp3",
-            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【幸福額度】Official Music Video.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/蘇打綠 sodagreen -【幸福額度】Official Music Video.lrc"
         },
         {
             name: "蘇打綠 - 故事",
             artist: "蘇打綠",
             src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【故事】Official Music Video.mp3",
-            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【故事】Official Music Video.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/蘇打綠 sodagreen -【故事】Official Music Video.lrc"
         },
         {
             name: "蘇打綠 - 燕窩",
             artist: "蘇打綠",
             src: "https://joydragonx.github.io/music/中文歌/.mp3",
-            lrc: "https://joydragonx.github.io/music/中文歌/.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/.lrc"
         },
         {
             name: "蘇打綠 - 說了再見以後",
             artist: "蘇打綠",
             src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【說了再見以後】Official Music Video.mp3",
-            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【說了再見以後】Official Music Video.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/蘇打綠 sodagreen -【說了再見以後】Official Music Video.lrc"
         },
         {
             name: "蘇打綠 sodagreen - 近未來",
             artist: "蘇打綠",
             src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【近未來】Official Music Video.mp3",
-            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【近未來】Official Music Video.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/蘇打綠 sodagreen -【近未來】Official Music Video.lrc"
         },
         {
             name: "牧野由依 - Reset 重啟咲良田 OP1",
             artist: "牧野由依",
             src: "https://joydragonx.github.io/music/日本歌/牧野由依 Reset.mp3",
-            lrc: "https://joydragonx.github.io/music/日本歌/牧野由依 Reset.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/牧野由依 Reset.lrc"
         },
         {
             name: "WEAVER - だから僕は僕を手放す 重啟咲良田 OP2",
             artist: "WEAVER",
             src: "https://joydragonx.github.io/music/日本歌/Sakurada Reset OP2WEAVER  Dakara Boku wa Boku wo Tebanasuだから僕は僕を手放す.mp3",
-            lrc: "https://joydragonx.github.io/music/日本歌/Sakurada Reset OP2WEAVER  Dakara Boku wa Boku wo Tebanasuだから僕は僕を手放す.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/Sakurada Reset OP2WEAVER  Dakara Boku wa Boku wo Tebanasuだから僕は僕を手放す.lrc"
         },
         {
             name: "山中拓也 - トナリアウ 重啟咲良田 ED1",
             artist: "山中拓也",
             src: "https://joydragonx.github.io/music/日本歌/トナリアウSakurada Reset ED.mp3",
-            lrc: "https://joydragonx.github.io/music/日本歌/.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/.lrc"
         },
         {
             name: "牧野由依 - Colors of happiness 重啟咲良田 ED2",
             artist: "牧野由依",
             src: "https://joydragonx.github.io/music/日本歌/Colors of happiness Sakurada Reset ED.mp3",
-            lrc: "https://joydragonx.github.io/music/日本歌/Colors of happiness Sakurada Reset ED.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/Colors of happiness Sakurada Reset ED.lrc"
         },
         {
             name: "中井雅子 - もうすでに失ったもの 重啟咲良田OST",
             artist: "中井雅子",
             src: "https://joydragonx.github.io/music/日本歌/Sakurada Reset Track 02 What Ive Lost Already Rayons もうすでに失ったもの 重啟咲良田.mp3",
-            lrc: "https://joydragonx.github.io/music/日本歌/Sakurada Reset Track 02 What Ive Lost Already Rayons もうすでに失ったもの 重啟咲良田.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/Sakurada Reset Track 02 What Ive Lost Already Rayons もうすでに失ったもの 重啟咲良田.lrc"
         },
         {
             name: "鈴木このみ - Theater of Life DECA-DENCE OP",
             artist: "鈴木このみ",
             src: "https://joydragonx.github.io/music/日本歌/DECADENCE OP FullAMV Theater of Lifeby Konomi Suzuki HD.mp3",
-            lrc: "https://joydragonx.github.io/music/日本歌/DECADENCE OP FullAMV Theater of Lifeby Konomi Suzuki HD.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/DECADENCE OP FullAMV Theater of Lifeby Konomi Suzuki HD.lrc"
         },
         {
             name: "伊東歌詞太郎 - 記憶の箱舟",
             artist: "伊東歌詞太郎",
             src: "https://joydragonx.github.io/music/日本歌/伊東歌詞太郎記憶の箱舟TVアニメデカダンスEDテーマMusic Video.mp3",
-            lrc: "https://joydragonx.github.io/music/日本歌/伊東歌詞太郎記憶の箱舟TVアニメデカダンスEDテーマMusic Video.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/伊東歌詞太郎記憶の箱舟TVアニメデカダンスEDテーマMusic Video.lrc"
         },
         {
             name: "MAN WITH A MISSION - Winding Road 黃金神威 OP1",
             artist: "MAN WITH A MISSION",
             src: "https://joydragonx.github.io/music/日本歌/MAN WITH A MISSIONWinding Road  黃金神威OP 中日英字幕.mp3",
-            lrc: "https://joydragonx.github.io/music/日本歌/MAN WITH A MISSIONWinding Road  黃金神威OP 中日英字幕.lrc"
+            lrc: "https://joydragonx.github.io/music/lyric/MAN WITH A MISSIONWinding Road  黃金神威OP 中日英字幕.lrc"
         }
     ];
