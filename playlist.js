3a51a120bf64d25b3dc3f1708455085d32000057
JoyDragonX <57489277+JoyDragonX@users.noreply.github.com>
2021-08-14 22:13:04 +0800

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,4 +1,4 @@
-rackList =
+trackList =
     [
         {
             name: "白安 - 是什麼讓我遇見這樣的你",
@@ -7,21 +7,75 @@ rackList =
             lrc: "https://joydragonx.github.io/music/lyric/Ann白安【是什麼讓我遇見這樣的你What brings me to you】 MV官方完整版.lrc"
         },
         {
-            name: "Maroon 5 - Beautiful Mistakes ft. Megan Thee Stallion",
-            artist: "Maroon 5",
-            src: "musics/Maroon 5 - Beautiful Mistakes ft. Megan Thee Stallion (Official Music Video).mp3",
-            lrc: "lyrics/Maroon 5 - Beautiful Mistakes ft. Megan Thee Stallion (Official Music Video).lrc"
+            name: "吳青峰 - 起風了",
+            artist: "吳青峰",
+            src: "https://joydragonx.github.io/music/中文歌/吳青峰《起風了》高音質 動態歌詞版MV.mp3",
+            lrc: "https://joydragonx.github.io/music/中文歌/吳青峰《起風了》高音質 動態歌詞版MV.lrc"
         },
         {
-            name: "五月天 - 青空未來",
-            artist: "MAYDAY 五月天阿信",
-            src: "musics/MAYDAY 五月天阿信 [ 青空未來 Future ] Official Music Video.mp3",
-            lrc: "lyrics/MAYDAY 五月天阿信 [ 青空未來 Future ] Official Music Video.lrc"
+            name: "蘇打綠 - 小情歌",
+            artist: "蘇打綠",
+            src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen - 【小情歌 (Endless Story Live) 】 Official Music Video.mp3",
+            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen - 【小情歌 (Endless Story Live) 】 Official Music Video.lrc"
         },
         {
-            name: "盧廣仲 - 刻在我心底的名字",
-            artist: "盧廣仲 ",
-            src: "musics/盧廣仲 Crowd Lu 【刻在我心底的名字 Your Name Engraved Herein】 Official Music Video （刻在你心底的名字電影主題曲）.mp3",
-            lrc: "lyrics/盧廣仲 - 刻在我心底的名字.lrc"
-        }
+            name: "蘇打綠 - 下雨的夜晚",
+            artist: "蘇打綠",
+            src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【下雨的夜晚 Live】Official Music Video.mp3",
+            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【下雨的夜晚 Live】Official Music Video.lrc"
+        },
+        {
+            name: "蘇打綠 - 你在煩惱什麼",
+            artist: "蘇打綠",
+            src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【你在煩惱什麼 Whats The Trouble On Your Mind】Official Music Video.mp3",
+            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【你在煩惱什麼 Whats The Trouble On Your Mind】Official Music Video.lrc"
+        },
+        {
+            name: "蘇打綠 - 再遇見",
+            artist: "蘇打綠",
+            src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【再遇見】Official Music Video.mp3.mp3",
+            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【再遇見】Official Music Video.mp3.lrc"
+        },
+        {
+            name: "蘇打綠 sodagreen -【十年一刻】Official Music Video.mp3",
+            artist: "蘇打綠",
+            src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【十年一刻】Official Music Video.mp3.mp3",
+            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【十年一刻】Official Music Video.mp3.lrc"
+        },
+        {
+            name: "蘇打綠 - 左邊",
+            artist: "蘇打綠",
+            src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【左邊】Official Music Video.mp3",
+            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【左邊】Official Music Video.lrc"
+        },
+        {
+            name: "蘇打綠 - 幸福額度",
+            artist: "蘇打綠",
+            src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【幸福額度】Official Music Video.mp3",
+            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【幸福額度】Official Music Video.lrc"
+        },
+        {
+            name: "蘇打綠 - 故事",
+            artist: "蘇打綠",
+            src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【故事】Official Music Video.mp3",
+            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【故事】Official Music Video.lrc"
+        },
+        {
+            name: "蘇打綠 - 燕窩",
+            artist: "蘇打綠",
+            src: "https://joydragonx.github.io/music/中文歌/.mp3",
+            lrc: "https://joydragonx.github.io/music/中文歌/.lrc"
+        },
+        {
+            name: "蘇打綠 - 說了再見以後",
+            artist: "蘇打綠",
+            src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【說了再見以後】Official Music Video.mp3",
+            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【說了再見以後】Official Music Video.lrc"
+        },
+        {
+            name: "蘇打綠 sodagreen - 近未來",
+            artist: "蘇打綠",
+            src: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【近未來】Official Music Video.mp3",
+            lrc: "https://joydragonx.github.io/music/中文歌/蘇打綠 sodagreen -【近未來】Official Music Video.lrc"
+        },
     ];
