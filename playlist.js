e6f478945ad69b81628f4c9235328ccf25103111
JoyDragonX <57489277+JoyDragonX@users.noreply.github.com>
2021-08-14 21:51:52 +0800

    Create playlist.js

---
+++ playlist.js
@@ -0,0 +1,27 @@
+rackList =
+    [
+        {
+            name: "白安 - 是什麼讓我遇見這樣的你",
+            artist: "白安",
+            src: "https://joydragonx.github.io/music/中文歌/Ann白安【是什麼讓我遇見這樣的你What brings me to you】 MV官方完整版.mp3",
+            lrc: "https://joydragonx.github.io/music/lyric/Ann白安【是什麼讓我遇見這樣的你What brings me to you】 MV官方完整版.lrc"
+        },
+        {
+            name: "Maroon 5 - Beautiful Mistakes ft. Megan Thee Stallion",
+            artist: "Maroon 5",
+            src: "musics/Maroon 5 - Beautiful Mistakes ft. Megan Thee Stallion (Official Music Video).mp3",
+            lrc: "lyrics/Maroon 5 - Beautiful Mistakes ft. Megan Thee Stallion (Official Music Video).lrc"
+        },
+        {
+            name: "五月天 - 青空未來",
+            artist: "MAYDAY 五月天阿信",
+            src: "musics/MAYDAY 五月天阿信 [ 青空未來 Future ] Official Music Video.mp3",
+            lrc: "lyrics/MAYDAY 五月天阿信 [ 青空未來 Future ] Official Music Video.lrc"
+        },
+        {
+            name: "盧廣仲 - 刻在我心底的名字",
+            artist: "盧廣仲 ",
+            src: "musics/盧廣仲 Crowd Lu 【刻在我心底的名字 Your Name Engraved Herein】 Official Music Video （刻在你心底的名字電影主題曲）.mp3",
+            lrc: "lyrics/盧廣仲 - 刻在我心底的名字.lrc"
+        }
+    ];
